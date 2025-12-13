fe8cc6e90d487178cbfa066802ac669f81c9a782
aayushvermaa <aayush.verma@opstree.com>
2025-12-13 23:43:00 +0530

    update base shell function

--- BP-BASE-SHELL-STEPS/log-functions.sh
+++ BP-BASE-SHELL-STEPS/log-functions.sh
@@ -3,13 +3,14 @@
 GREEN="32m"
 RED="31m"
 YELLOW="1;33m"
+CYAN="36m"  # Color for debugging
 
 COLOR_START="\e["
 COLOR_END="\e[0m"
 
 function logColoredMessage() {
-    COLOR=$1
-    LOG_LEVEL=$2
+    COLOR="$1"
+    LOG_LEVEL="$2"
     MESSAGE="$3"
 
     CURRENT_DATE=$(date "+%D: %T")
@@ -19,16 +20,23 @@ function logColoredMessage() {
 function logInfoMessage() {
     MESSAGE="$1"
 
-    logColoredMessage ${GREEN} INFO "${MESSAGE}"
+    logColoredMessage "${GREEN}" INFO "${MESSAGE}"
 }
 
 function logErrorMessage() {
     MESSAGE="$1"
 
-    logColoredMessage ${RED} ERROR "${MESSAGE}"
+    logColoredMessage "${RED}" ERROR "${MESSAGE}"
 }
 
 function logWarningMessage() {
     MESSAGE="$1"
-    logColoredMessage ${YELLOW} WARNING "${MESSAGE}"
+    logColoredMessage "${YELLOW}" WARNING "${MESSAGE}"
+}
+
+# Function to log debug messages if DEBUG is enabled
+function logDebugMessage() {
+    if [[ "$(echo "$DEBUG" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+        logColoredMessage "${CYAN}" DEBUG "$1"
+    fi
 }
